fb5aee59f99bcf39642c2732d4b139db82e9d14d
István Gansperger <gansperger@gmail.com>
2015-09-22 11:53:23 +0200

    Update ws.charting to use ui.next

--- WebSharper.Data.Test/Client.fs
+++ WebSharper.Data.Test/Client.fs
@@ -1,7 +1,6 @@
 namespace WebSharper.Data.Test
 
 open WebSharper
-open WebSharper.JavaScript
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Html
 open WebSharper.UI.Next.Client
@@ -15,28 +14,21 @@ module Client =
     let data = WorldBank.GetDataContext()
 
     let Main =
-
-        let d = div []
-
         let chrt =
             async {
                 let! school = data.Countries.Hungary.Indicators.``School enrollment, tertiary (% gross)``
 
                 let data = Seq.zip (Seq.map string school.Years) school.Values
-                let chrt =
+                return
                     Chart.Line(data)
                     |> fun c -> Renderers.ChartJs.Render(c, Window = 10)
-                d.Append <| Doc.Static chrt.Dom
-                chrt.Render()
             }
             |> View.Const
             |> View.MapAsync id
-            |> View.Map (fun () -> Doc.Empty)
             |> Doc.EmbedView
 
         Doc.Concat [
             h3 [ text "Tertiary school enrollment in Hungary in the last 10 years, (% gross)" ]
-            d
             chrt
         ]
         |> Doc.RunById "main"
